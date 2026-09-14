043f967c9c57ad4fbf10301233daaffe9d91f921
control-ua <zenikov4000@gmail.com>
2026-09-14 23:22:04 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='solarflow-v15-6-estimate-files-20260914';
+const CACHE_NAME='solarflow-v15-7-sound-notifications-20260914';
 const CORE=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE).catch(()=>{})));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
